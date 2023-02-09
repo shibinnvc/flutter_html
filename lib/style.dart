265050e773253ecd17a6c39d796b85be94079356
shibinnvc <109967452+shibinnvc@users.noreply.github.com>
2023-02-09 22:27:55 +0530

    Update style.dart

--- lib/style.dart
+++ lib/style.dart
@@ -380,8 +380,8 @@ class Style {
     LineHeight? finalLineHeight = child.lineHeight != null
         ? child.lineHeight?.units == "length"
             ? LineHeight(child.lineHeight!.size! /
-                (finalFontSize == null ? 22 : finalFontSize.value) *
-                3.5)
+                (finalFontSize == null ? 14 : finalFontSize.value) *
+                1.2)
             : child.lineHeight
         : lineHeight;
 
